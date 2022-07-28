be0bab0577d4d9490b47944801d63d2a8296fdc4
CrowBarJonns <macalite@flashgiver.com>
2022-07-28 22:36:21 +1200

    Updated App.js MOTD

--- cise-react-learn/src/App.js
+++ cise-react-learn/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Agile is about <code>Values</code> and principles.
         </p>
         <a
           className="App-link"
